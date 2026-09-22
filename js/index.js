caff267b880b92f577e2e20414054aba1bc72f90
Rasmus Madsen <ram006@edu.zealand.dk>
2026-09-22 10:18:27 +0200

    Added functions create and add new list elements to lists

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
 //Write your Javascript code here
+
+function CreateNewLiElement(className, id, textNode){
+    let newElement = document.createElement("li");
+    newElement.setAttribute("id",id)
+    newElement.setAttribute("class",className);
+    newtextnode = document.createTextNode(textNode);
+    newElement.appendChild(newtextnode);
+    return newElement;
+}
+
+function AddLiElementToList(listId,newListElement){
+let startNode = document.getElementById(listId);
+startNode.appendChild(newListElement);
+}
+
+
+
+let appel = CreateNewLiElement("healthy",4,"appel");
+let chipper = CreateNewLiElement("unhealthy",4,"chipper")
+AddLiElementToList("listUnhealthy",chipper);
+AddLiElementToList("listHealthy",appel);
+
+
+
 console.log("Shoppinglist")
